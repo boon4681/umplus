f7a2019c040e5d5f5c9ec55ea1ad42fd9781dcff
boon4681 <46645814+boon4681@users.noreply.github.com>
2022-11-30 06:39:39 +0700

    update

--- admin-site/vite.config.ts
+++ admin-site/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true
       },
-      'download':{
+      '/download':{
         target:'http://localhost:3000',
         changeOrigin: true
       },
